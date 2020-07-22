852684aa3db72524b8d1cc3c8976789d3fd1509c
Alexander Omelchuk <cattivole@icloud.com>
2020-07-22 20:26:45 +0300

    Initial Commit

---
+++ MotionEffectExperiment/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MotionEffectExperiment
+//
+//  Created by Александр Омельчук on 22.07.2020.
+//  Copyright © 2020 Александр Омельчук. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
