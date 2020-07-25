c7e942b3e315f05d3219c86617c3f70fe17cd415
Alexander Omelchuk <cattivole@icloud.com>
2020-07-25 12:56:01 +0300

    Refactoring

--- MotionEffectExperiment/ViewController.swift
+++ MotionEffectExperiment/ViewController.swift
@@ -1,61 +1,75 @@
 import UIKit
 
 class ViewController: UIViewController {
+  
+  let motionEffectButton: UIButton = {
+    let button = UIButton()
+    button.frame = CGRect(x: 0, y: 0, width: 250, height: 50)
+    button.layer.cornerRadius = 15
+    button.backgroundColor = .red
+    button.setTitle("Press Me", for: .normal)
+    button.setTitleColor(.white, for: .normal)
     
-    let motionEffectButton: UIButton = {
-        let button = UIButton()
-        button.frame = CGRect(x: 0, y: 0, width: 250, height: 50)
-        button.layer.cornerRadius = 15
-        button.backgroundColor = .red
-        button.setTitle("Press Me", for: .normal)
-        button.setTitleColor(.white, for: .normal)
-        
-        button.layer.borderColor = UIColor.gray.cgColor
-        button.layer.borderWidth = 1
-        return button
-    }()
+    button.layer.borderColor = UIColor.gray.cgColor
+    button.layer.borderWidth = 1
+    button.addParalaxEffect(.moveShadow)
+    return button
+  }()
+  
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    view.backgroundColor = .white
+    makeLayout()
+  }
+  
+  private func makeLayout() {
+    view.addSubview(motionEffectButton)
+    motionEffectButton.center = view.center
+  }
+}
+
+extension UIButton {
+  
+  enum ParalaxEffectType {
+    case moveButton
+    case moveShadow
+  }
+  
+  func addParalaxEffect(
+    _ type: ParalaxEffectType,
+    relative: Int = 16,
+    shadow: Bool = true,
+    radius: CGFloat = 5,
+    opacity: Float = 0.8
+  ) {
     
     let horisontalEffect = UIInterpolatingMotionEffect(
-        keyPath: "center.x",//"layer.shadowOffset.width",
-        type: .tiltAlongHorizontalAxis
+      keyPath: type == .moveButton ? "center.x" : "layer.shadowOffset.width",
+      type: .tiltAlongHorizontalAxis
     )
     
     let verticalEffect = UIInterpolatingMotionEffect(
-        keyPath: "center.y",//"layer.shadowOffset.height",
-        type: .tiltAlongVerticalAxis
+      keyPath: type == .moveButton ? "center.y" : "layer.shadowOffset.height",
+      type: .tiltAlongVerticalAxis
     )
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        view.backgroundColor = .white
-        makeLayout()
-        //addShadow()
-        addMovingEffect()
-    }
     
-    private func addShadow() {
-        motionEffectButton.layer.shadowOpacity = 0.8
-        motionEffectButton.layer.shadowRadius = 15
-        motionEffectButton.layer.shadowColor = UIColor.black.cgColor
-        motionEffectButton.layer.shadowOffset = CGSize(width: 0, height: 26)
-    }
+    let effectGroup = UIMotionEffectGroup()
     
-    private func makeLayout() {
-        view.addSubview(motionEffectButton)
-        motionEffectButton.center = view.center
+    horisontalEffect.minimumRelativeValue = relative
+    horisontalEffect.maximumRelativeValue = -relative
+    
+    verticalEffect.minimumRelativeValue = relative
+    verticalEffect.maximumRelativeValue = -relative
+    
+    effectGroup.motionEffects = [horisontalEffect, verticalEffect]
+    
+    self.addMotionEffect(effectGroup)
+    
+    if type == .moveShadow || shadow {
+      self.layer.shadowOpacity = opacity
+      self.layer.shadowRadius = radius
+      self.layer.shadowColor = UIColor.black.cgColor
+      self.layer.shadowOffset = CGSize(width: relative, height: relative)
     }
-
-    private func addMovingEffect() {
-        let effectGroup = UIMotionEffectGroup()
-        
-        horisontalEffect.minimumRelativeValue = 50
-        horisontalEffect.maximumRelativeValue = -50
-        
-        verticalEffect.minimumRelativeValue = 50
-        verticalEffect.maximumRelativeValue = -50
-        
-        effectGroup.motionEffects = [horisontalEffect, verticalEffect]
-        
-        motionEffectButton.addMotionEffect(effectGroup)
-    }
+  }
 }
